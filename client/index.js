f74bbcbb1dff93e309af21a17a6986bdfe47e3f6
Dave Nadler <davidnadler9637@gmail.com>
2021-03-03 13:49:06 -0600

    getting things going!

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+render(
+  <p>Hey There!</p>,
+  document.getElementById('root')
+);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+
+
+
+
+module.exports = {
+  mode: 'production',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    publicPath: '/build/',
+    proxy: {
+      '/': 'http://localhost:3000'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      // {
+      //   test: /.(css|scss)$/,
+      //   exclude: [/node_modules/, /client\/stylesheets\/modules/],
+      //   use: ['style-loader', 'css-loader', 'sass-loader'],
+      // },
+    ],
+  }
+}
